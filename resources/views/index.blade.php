81c24cb5905f3c06b9c4dd6ed8153cadaebe5052
Ian Clemence <ianclemence17@gmail.com>
2022-09-20 12:55:22 +0700

    Add styling

---
+++ resources/views/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+            Popular Games
+        </h2>
+        <div class="popular-games text-sm grid drid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="/img/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semilbold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+        </div>
+
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed lg:w-3/4 mr-0 lg:mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                   Recently Reviewed
+                </h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/img/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px">
+                                <div class="font-semilbold text-xs flex justify-center items-center h-full">80%</div>
+
+                            </div>
+                        </div>
+                        <div class="ml-6 lg:ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy 7 Remake</a>
+                            <div class="text-gray-400 mt-1">Playsitaion 4</div>
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quam, inventore quidem temporibus sequi eum cum perferendis nesciunt harum, deleniti ipsum exercitationem numquam laborum nihil corrupti. Ut incidunt eveniet corporis labore libero, deleniti facere iure ipsum, quod quia accusantium dicta animi ipsa numquam voluptatem quisquam illo ducimus vel similique sapiente!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="most-anticiated lg:w-1/4 mt-12 lg:mt-0">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                    Most Anticipated
+                 </h2>
+                 <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/img/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">
+                                Sept 16, 2022
+                            </div>
+                        </div>
+
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+@endsection
